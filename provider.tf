6519f8a276c8d4d53fb02004e6d5965b09d1e7f9
Carlos Daniel Martinez <55921881+Daniel-1A@users.noreply.github.com>
2026-05-11 19:13:45 -0600

    Task 4: demonstrate DynamoDB lock contention

--- provider.tf
+++ provider.tf
@@ -4,8 +4,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    time = {
+      source  = "hashicorp/time"
+      version = "~> 0.11"
+    }
   }
 }
+
 provider "aws" {
   region = var.region
 }
